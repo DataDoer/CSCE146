0188753be663ad8d5e13b5bcd3f7b5b89f9a4bc2
DataDoer <noellep@email.sc.edu>
2017-09-14 12:14:09 -0400

    Add files via upload

---
+++ September 14/BuiltInJavaStuff.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class BuiltInJavaStuff {
+	public static void main(String[] args)
+	{
+		LinkedList<Taco> tacosLL = new LinkedList<Taco>();
+		tacosLL.remove();
+
+		ArrayList<Taco> tacoAL = new ArrayList<Taco>();
+		
+		Taco aTaco = tacoAL.get(4);
+	
+	}
+	
+
+}
